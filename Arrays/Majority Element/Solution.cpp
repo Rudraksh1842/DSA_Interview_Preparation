61fc5f266f22bbbcf14205b009ebe8ffe12ea8ee
Rudraksh Soni <94983587+Rudraksh1842@users.noreply.github.com>
2026-02-26 23:29:06 +0530

    Create Solution.cpp

---
+++ Arrays/Majority Element/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int i:nums){
+            m[i]++;
+        }
+        for(auto i:m){
+            if(i.second>(nums.size()/2)){
+                return i.first;
+            }
+        }
+        return 0;
+    }
+};
